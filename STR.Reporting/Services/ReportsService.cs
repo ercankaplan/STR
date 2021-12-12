9e6b02ac9e3a00fa29f59f313ec2e8144c6a181c
ercank <ercank@codebase.com.tr>
2021-12-12 23:24:48 +0300

    Queue consumer için console app eklendi. Rapor nesneleri için db connection ve Repost Service implementasyonu yapıldı.

---
+++ STR.Reporting/Services/ReportsService.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using STR.Data.Models;
+using STR.Data.Models.Ef;
+using STR.Data.Models.Ef.EfContext;
+using STR.Reporting.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STR.Reporting.Services
+{
+    public class ReportsService : IReportsService
+    {
+
+        private readonly STRDbContext dbContext;
+  
+
+
+        public ReportsService(STRDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+  
+
+        }
+
+        public async Task<(bool IsSuccess, string Error)> ExecuteReportAsync(Guid id)
+        {
+            try
+            {
+
+                ReportRequest reportRequest =  dbContext.ReportRequest.Where(o => o.Id == id).SingleOrDefault();
+
+                //TODO execute sp get result 
+
+                ReportResult reportResult = new ReportResult()
+                {
+                    Id = Guid.NewGuid(),
+                    ReportRequestId = reportRequest.Id,
+                    Result = "Dummy Result",
+                    CreatedTime = DateTime.Now
+                };
+
+                dbContext.ReportResult.Add(reportResult);
+
+                reportRequest.Status = (byte)EnumReportStatus.Ready;
+
+                //Task.Delay(1000);
+
+
+                await dbContext.SaveChangesAsync();
+
+
+                return (true, "Executed Report");
+            }
+            catch (Exception ex)
+            {
+
+           
+
+                await this.UpdateReportStatusAsync(id, EnumReportStatus.Fail);
+
+                return (false, ex.Message);
+            }
+        }
+
+        public async Task<(bool IsSuccess, string Error)> UpdateReportStatusAsync(Guid id, EnumReportStatus newStatus)
+        {
+            try
+            {
+
+                ReportRequest reportRequest =  dbContext.ReportRequest.Where(o => o.Id == id).SingleOrDefault();
+
+                if (reportRequest == null)
+                    throw new ApplicationException("Not Found ReportRequest");
+
+                reportRequest.Status = (byte)newStatus;
+                reportRequest.UpdateTime = DateTime.Now;
+
+                await dbContext.SaveChangesAsync();
+
+                return (true, "Updated Report Status");
+            }
+            catch (Exception ex)
+            {
+
+         
+                return (false, ex.Message);
+            }
+        }
+
+       
+
+    }
+}
